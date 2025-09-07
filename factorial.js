afafc44a5045fd909c2eba501f66ae2586e198e3
Yiel-Cadavoz <cadavoz.arrieldenver@gmail.com>
2025-09-08 07:49:48 +0800

    Added =

--- factorial.js
+++ factorial.js
@@ -12,11 +12,11 @@ console.log("2. Compile Factorial");
 console.log("3. Exit");
 
 rl.question("Enter your choice (1-3)" , choice =>{
-    if(choice=="1"){
+    if(choice==="1"){
         console.log("Hello There!");
-    } else if(choice=="2"){
+    } else if(choice==="2"){
         console.log("Factorial");
-    } else if(choice=="3"){
+    } else if(choice==="3"){
         console.log("Exit");
     } else{
         console.log("Invalid choice. Please try again");
