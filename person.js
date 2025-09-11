1f49406934bb340bf8363baa04498cbe255aac73
Yiel-Cadavoz <cadavoz.arrieldenver@gmail.com>
2025-09-11 08:35:28 +0800

    Added person.js

---
+++ person.js
@@ -0,0 +1,25 @@
+class Person{
+    constructor(name,age,occupation){
+        this.name = name;
+        this.age = age;
+        this.occupation = occupation;
+
+    }
+
+    getName(){
+        console.log(this.name);
+    }
+    displayInfo(){
+        console.log("Name: " + this.name);
+        console.log("Age: " + this.age);
+        console.log("Occupation: " + this.occupation);
+        
+    }
+}
+
+const person1 = new Person("Coco Martin","24", "Masahista");
+const person2 = new Person("Tanggol","29", "ex mayor");
+
+
+person1.getName();
+person2.displayInfo();
